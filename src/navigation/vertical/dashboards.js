3ba567e8a2d610313688745b8316b050527d09d4
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-03-26 13:11:36 +0530

    init

---
+++ src/navigation/vertical/dashboards.js
@@ -0,0 +1,70 @@
+// ** Icons Import
+import {
+    Home,
+    Circle,
+    Users,
+    Book,
+    Settings,
+    Box,
+    Archive,
+    UserPlus,
+    Trello,
+    Briefcase,
+    BookOpen,
+    CreditCard
+} from 'react-feather'
+
+export default [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        icon: <Home size={12}/>,
+        navLink: '/dashboard'
+    },
+    {
+        id: 'orders',
+        title: 'Orders',
+        icon: <CreditCard size={12}/>,
+        navLink: '/orders'
+    },
+    {
+        id: 'mainCat',
+        title: 'Main category',
+        icon: <BookOpen size={12}/>,
+        navLink: '/main-category',
+        children: [
+            {
+                id: 'viewMainCat',
+                title: 'view',
+                icon: <Circle size={12}/>,
+                navLink: '/main-category/view'
+            },
+            {
+                id: 'orderView',
+                title: 'create',
+                icon: <Circle size={12}/>,
+                navLink: '/main-category/create'
+            }
+        ]
+    },
+    {
+        id: 'subCat',
+        title: 'Sub category',
+        icon: <Briefcase size={12}/>,
+        navLink: '/sub-category',
+        children: [
+            {
+                id: 'viewSubCat',
+                title: 'view',
+                icon: <Circle size={12}/>,
+                navLink: '/main-category/view'
+            },
+            {
+                id: 'orderView',
+                title: 'create',
+                icon: <Circle size={12}/>,
+                navLink: '/main-category/create'
+            }
+        ]
+    }
+]
