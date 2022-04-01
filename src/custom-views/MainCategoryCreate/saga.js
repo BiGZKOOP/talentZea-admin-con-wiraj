939890958ac2747512344cc73a3620e574561236
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-04-01 14:52:32 +0530

    main category redux flow finished

--- src/custom-views/MainCategoryCreate/saga.js
+++ src/custom-views/MainCategoryCreate/saga.js
@@ -1,11 +1,27 @@
+import * as actionTypes from "./constants"
+import axios from "../../axios/axios"
+import {put, takeLatest} from "redux-saga/effects"
+import {createMainCatSuccess} from "./actions"
+
+const createMainServiceAsync = async (data) => {
+
+    return await axios.post("/main-service", data).then(res => res).catch(err => {
+        console.error(err.message)
+    })
+}
+
 ////////////////
 //ASYNC FINISHED
 ////////////////
 
 export function* createMainCatCB(action) {
 
-    try {
+    const {payload} = action
 
+    try {
+        const res = yield put(createMainServiceAsync, payload)
+        console.log(res)
+        yield put(createMainCatSuccess(res.data))
     } catch (err) {
         console.error(err.message)
     }
@@ -13,6 +29,7 @@ export function* createMainCatCB(action) {
 
 
 function* watchCreateMainCatSagas() {
+    yield takeLatest(actionTypes.CREATE_MAIN_SERVICE_LISTEN, createMainCatCB)
 }
 
 const createMainCatSagas = [watchCreateMainCatSagas]
