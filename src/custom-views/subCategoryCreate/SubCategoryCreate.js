36351f4f003b6ae3dc00bd5526b72b92ad8400ac
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-04-09 22:17:36 +0530

    faq card & ui components created.

--- src/custom-views/subCategoryCreate/SubCategoryCreate.js
+++ src/custom-views/subCategoryCreate/SubCategoryCreate.js
@@ -1,9 +1,22 @@
-import {Button, Card, Col, Form, Input, Label, Row, Spinner} from "reactstrap"
+import {
+    Accordion,
+    AccordionBody,
+    AccordionHeader, AccordionItem,
+    Button,
+    Card, CardBody, CardFooter, CardHeader,
+    Col,
+    Form,
+    Input,
+    Label,
+    Row,
+    Spinner
+} from "reactstrap"
 import {useFormik} from "formik"
 import {useEffect, useState} from "react"
 import "../../assets/css/mainCategory.css"
-import {PlusSquare, Upload} from "react-feather"
-import {fireAlertError} from "../../utility/customUtils"
+import "../../assets/css/serviceViews.css"
+import {Circle, Delete, Gift, PlusSquare, Upload} from "react-feather"
+import {fireAlertCustom, fireAlertError} from "../../utility/customUtils"
 import {useDispatch, useSelector} from "react-redux"
 import {getAllMainCatListen} from "../MainCategoryView/actions"
 // eslint-disable-next-line no-unused-vars
@@ -20,6 +33,12 @@ const SubCategoryCreate = () => {
     const [image2, setImage2] = useState()
     const [image3, setImage3] = useState()
 
+    const [faqQuestion, setFaqQuestion] = useState("")
+    const [faqAnswer, setFaqAnswer] = useState("")
+
+    //This contains all the FAQ object array
+    const [faq, setFaq] = useState([])
+
     // eslint-disable-next-line no-unused-vars
     const dispatch = useDispatch()
 
@@ -35,6 +54,26 @@ const SubCategoryCreate = () => {
             image2,
             image3
         }
+    }
+
+    //Use this to cook the FAQ object (must contains question & answer)
+    const addFAQ = (e) => {
+        e.preventDefault()
+
+        if (!faqQuestion) {
+            fireAlertCustom("Hmm...", "You need to add a question !")
+            return
+        }
+
+        if (!faqAnswer) {
+            fireAlertCustom("Hmm...", "You need to add a answer !")
+            return
+        }
+
+        setFaq(faq.concat({
+            question: faqQuestion,
+            answer: faqAnswer
+        }))
     }
 
     const validate = (values) => {
@@ -136,67 +175,124 @@ const SubCategoryCreate = () => {
         }
     }
 
+    // eslint-disable-next-line no-unused-vars
+    const removeFAQ = (e, index) => {
+        e.preventDefault()
+        alert(index)
+    }
+
     useEffect(() => {
         dispatch(getAllMainCatListen())
     }, [])
 
-    return <Card className="p-1">
+    return <div className="p-1">
         <Form onSubmit={formik.handleSubmit}>
-            <Col>
-                <Label htmlFor="main" className="text-small mb-1">Main topic</Label>
-                <Input
-                    id="mainTopic"
-                    name="mainTopic"
-                    onChange={formik.handleChange}
-                    value={formik.values.mainTopic}
-                    placeholder="Enter main topic..."/>
-            </Col>
-            <Col className="mt-2">
-                <Label htmlFor="main" className="text-small mb-1">Service description</Label>
-                <Input
-                    type="textarea"
-                    id="description"
-                    name="description"
-                    onChange={formik.handleChange}
-                    value={formik.values.description}
-                    placeholder="Enter main topic..."/>
-            </Col>
-            <Col className="mt-2">
-                <Label htmlFor="main" className="text-small mb-1">Select </Label>
-                <Card className="w-25">
-                    <select
-                        className="custom-dropdown bg-instagram text-light"
-                        id="mainService"
-                        name="mainService"
+            <Card className="p-1">
+                <Col>
+                    <Label htmlFor="main" className="text-small mb-1">Main topic</Label>
+                    <Input
+                        id="mainTopic"
+                        name="mainTopic"
                         onChange={formik.handleChange}
-                        value={formik.values.mainService}
-                        placeholder="Select main service...">
-                        <option>Select main service</option>
-                        {
-                            mainCat?.map((e, index) => {
-                                if (mainCatLoading) return <option key={index}>Loading...</option>
-                                else return <option key={index} value={e._id}>{e.mainTopic}</option>
-                            })
-                        }
-                    </select>
-                </Card>
-            </Col>
-            <Col className="mt-5">
-                <div className="d-flex justify-content-between">
-                    <div>
-                        {handleImage1()}
-                        <input onChange={(e) => setImage1(e.target.files[0])} type="file" id="image1" hidden/>
+                        value={formik.values.mainTopic}
+                        placeholder="Enter main topic..."/>
+                </Col>
+                <Col className="mt-2">
+                    <Label htmlFor="main" className="text-small mb-1">Service description</Label>
+                    <Input
+                        type="textarea"
+                        id="description"
+                        name="description"
+                        onChange={formik.handleChange}
+                        value={formik.values.description}
+                        placeholder="Enter main topic..."/>
+                </Col>
+                <Col className="mt-2">
+                    <Label htmlFor="main" className="text-small mb-1">Select </Label>
+                    <Card className="w-25">
+                        <select
+                            className="custom-dropdown bg-instagram text-light"
+                            id="mainService"
+                            name="mainService"
+                            onChange={formik.handleChange}
+                            value={formik.values.mainService}
+                            placeholder="Select main service...">
+                            <option>Select main service</option>
+                            {
+                                mainCat?.map((e, index) => {
+                                    if (mainCatLoading) return <option key={index}>Loading...</option>
+                                    else return <option key={index} value={e._id}>{e.mainTopic}</option>
+                                })
+                            }
+                        </select>
+                    </Card>
+                </Col>
+                <Col className="mt-5">
+                    <div className="d-flex justify-content-between">
+                        <div>
+                            {handleImage1()}
+                            <input onChange={(e) => setImage1(e.target.files[0])} type="file" id="image1" hidden/>
+                        </div>
+                        <div>
+                            {handleImage2()}
+                            <input onChange={(e) => setImage2(e.target.files[0])} type="file" id="image2" hidden/>
+                        </div>
+                        <div>
+                            {handleImage3()}
+                            <input onChange={(e) => setImage3(e.target.files[0])} type="file" id="image3" hidden/>
+                        </div>
                     </div>
+                </Col>
+            </Card>
+            <div className="mt-5">
+                <Col>
+                    <Card className="p-1">
+                        <div className="mb-2 d-flex align-items-baseline">
+                            <Circle size={15} className="mr-1"/>
+                            <h3>Add FAQ</h3>
+                        </div>
+                        <div>
+                            <div className="mb-1">
+                                <Label htmlFor="question" className="text-small mb-1">Question</Label>
+                                <Input className="text" placeholder="question here..." value={faqQuestion}
+                                       onChange={e => setFaqQuestion(e.target.value)}/>
+                            </div>
+                            <div>
+                                <Label htmlFor="question" className="text-small mb-1">Answer</Label>
+                                <Input className="text" placeholder="answer here..." value={faqAnswer}
+                                       onChange={e => setFaqAnswer(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-end mt-2">
+                            <button className="btn btn-primary" onClick={(e) => addFAQ(e)}>Add FAQ</button>
+                        </div>
+                    </Card>
                     <div>
-                        {handleImage2()}
-                        <input onChange={(e) => setImage2(e.target.files[0])} type="file" id="image2" hidden/>
+                        <div style={{height: "400px"}} className="overflow-auto p-1 radius-10 shadow-inner">
+                            {
+                                faq.length > 0 ? faq.map((e, index) => {
+                                    return <div key={index} className="radius-10 mb-3 dashed-faq ">
+                                        <CardHeader className="p-1">{e.question}</CardHeader>
+                                        <hr/>
+                                        <CardBody className="p-1 d-flex">
+                                            <div className="flex-grow-10 word-break">
+                                                <p>{e.answer}</p>
+                                            </div>
+                                            <div className="flex-grow-1">
+                                                <button className="btn" onClick={(e) => removeFAQ(e, 1)}><Delete/>
+                                                </button>
+                                            </div>
+                                        </CardBody>
+                                    </div>
+                                }) : <div className="w-100 h-100 d-center animate__animated animate__bounce flex-column">
+                                    <Gift className="mb-2 text-primary"/>
+                                    <h2 className="f-Londrina text-success">Your FAQ will be shown here...</h2>
+                                </div>
+                            }
+                        </div>
                     </div>
-                    <div>
-                        {handleImage3()}
-                        <input onChange={(e) => setImage3(e.target.files[0])} type="file" id="image3" hidden/>
-                    </div>
-                </div>
-            </Col>
+                </Col>
+            </div>
             <Col className="d-flex justify-content-end mt-3 mb-2">
                 <button type="submit" className="btn btn-primary d-flex d-center text-small">
                     {
@@ -206,7 +302,7 @@ const SubCategoryCreate = () => {
                 </button>
             </Col>
         </Form>
-    </Card>
+    </div>
 }
 
 export default SubCategoryCreate
