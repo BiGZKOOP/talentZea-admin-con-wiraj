26779d32fe507bacaf35084486cb5bb6b4624d02
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-04-14 21:46:18 +0530

    view is init and table is created in order view

--- src/custom-views/OrderView/OrderView.js
+++ src/custom-views/OrderView/OrderView.js
@@ -1,6 +1,44 @@
+import {Card, Input, Table} from "reactstrap"
+import {Search} from "react-feather"
+
 const OrderView = () => {
 
-    return <h1>This is the order view </h1>
+    return <div>
+        <Card className="w-100 shadow-lg p-2">
+            <div className="w-25 d-flex">
+                <Input placeholder="search category..."/>
+                <button className="btn btn-outline-primary ml-2"><Search/></button>
+            </div>
+        </Card>
+        <Card className="mt-2 overflow-auto">
+            <table className="table table-hover ">
+                <thead>
+                <tr>
+                    <th scope="col">Order ID</th>
+                    <th scope="col">Order Date</th>
+                    <th scope="col">Service</th>
+                    <th scope="col">Customer name</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Status</th>
+                    <th scope="col" />
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>
+                        <button className="btn btn-primary">More details</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </Card>
+    </div>
 }
 
 export default OrderView
